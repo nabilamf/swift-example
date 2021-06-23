6b051719e623fe4b4bde669a39ac566e2ba4d0f6
Nabila Fissol <nabilafissol@gmail.com>
2021-06-23 16:56:05 +0800

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by NURUL NABILA MOHD FISSOL on 23/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/swift_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_exampleApp.swift
+//  Shared
+//
+//  Created by NURUL NABILA MOHD FISSOL on 23/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct swift_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
